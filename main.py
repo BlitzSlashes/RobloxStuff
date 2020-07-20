8c3fa6384048f082c4599a1cf87f9dc98dc87aff
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-07-20 18:29:37 +0300

    Fixed a error

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def search(username: str, opt=''):
         req = requests.get(
             f"https://www.roblox.com/search/users/results/?keyword={username}/UserSearchResults")
         res = req.json()
-        if opt:
-            ctx = {opt: i[opt] for i in res['UserSearchResults']}
-            return ctx
-        return res['UserSearchResults'][0]
-    except IndexError:
+        if res:
+            if opt:
+                ctx = {opt: i[opt] for i in res['UserSearchResults']}
+                return ctx
+            return res['UserSearchResults'][0]
+    except TypeError:
         return f"There isn't any info about {colored(username, color='red')}"
 
 
