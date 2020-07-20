f17d4bdc7bb6f73f7d962f75db7d5353d8513b7e
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-07-20 18:26:28 +0300

    Adding files

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from termcolor import colored
+import sys
+
+
+def search(username: str, opt=''):
+    try:
+        req = requests.get(
+            f"https://www.roblox.com/search/users/results/?keyword={username}/UserSearchResults")
+        res = req.json()
+        if opt:
+            ctx = {opt: i[opt] for i in res['UserSearchResults']}
+            return ctx
+        return res['UserSearchResults'][0]
+    except IndexError:
+        return f"There isn't any info about {colored(username, color='red')}"
+
+
+def main():
+    try:
+        username = sys.argv[1].lower()
+        option = sys.argv[2].lower()
+        options = {
+            'id': 'UserId',
+            'name': 'Name',
+            'about': 'Blurb',
+            'old_names': 'PreviousUserNamesCsv',
+            'status': 'IsOnline',
+            'main_group': 'PrimaryGroup'
+        }
+        res = search(username, opt=options[option])
+        return res
+    except IndexError:
+        return search(username)
+
+
+print(main())
